c8035aeec6766feca0faeb0408b7bf5f72a51e3d
PaulaSotoG <sotopaula01@gmail.com>
2024-10-20 18:54:01 +0200

    Update lab-sql-aggregation-and-transformation.sql

--- lab-sql-aggregation-and-transformation.sql
+++ lab-sql-aggregation-and-transformation.sql
@@ -1,3 +1,5 @@
+USE sakila
+
 -- You need to use SQL built-in functions to gain insights relating to the duration of movies:
 -- 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
 
